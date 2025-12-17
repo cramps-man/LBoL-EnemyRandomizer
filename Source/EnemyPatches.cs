f25e27066d7a26f3bf5a90aa4e7a003baabf4f04
cramps-man <cramps-man@hotmail.com>
2025-12-17 17:20:13 +1100

    Fix Sunny and make fairies use their spellcard

---
+++ Source/EnemyPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using LBoL.Core.Units;
+using LBoL.EntityLib.EnemyUnits.Character;
+
+namespace EnemyRandomizer
+{
+    [HarmonyPatch]
+    internal class EnemyPatches
+    {
+        [HarmonyPatch(typeof(Sunny), nameof(Sunny.OnEnterBattle))]
+        private static bool Prefix(Sunny __instance)
+        {
+            if (!__instance.GameRun.HasJadeBox<EnemyRandomizerJadebox>())
+                return true;
+            __instance.Spell();
+            return false;
+        }
+        [HarmonyPatch(typeof(Unit), "OnEnterBattle")]
+        private static bool Prefix(Unit __instance)
+        {
+            if (!__instance.GameRun.HasJadeBox<EnemyRandomizerJadebox>())
+                return true;
+            if (__instance is LightFairy lf)
+                lf.Spell();
+            return false;
+        }
+    }
+}
